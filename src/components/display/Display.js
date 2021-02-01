1adccdb94fc4b7fa0d889143595ece0effdbe297
ccarter-kenzie <christopher.carter@kenzie.academy>
2021-01-31 16:10:01 -0800

    message

---
+++ src/components/display/Display.js
@@ -0,0 +1,36 @@
+import { Component } from 'react'
+import Service from '../../services/service'
+
+
+class Display extends Component{
+  constructor(props) {
+    super(props);
+    this.client = new Service();
+    this.state = {
+      data: [],
+
+    }
+  }
+
+  getNewQuestion() {
+    return this.client.getQuestion().then(result => {
+      this.setState({
+        data: result.data
+      })
+    })
+  }
+  //when the component mounts, get a the first question
+  componentDidMount() {
+    this.getNewQuestion();
+  }
+
+  render() {
+    return (
+      <div>
+        {JSON.stringify(this.state.data)}
+      </div>
+    )
+  }
+}
+
+export default Display
